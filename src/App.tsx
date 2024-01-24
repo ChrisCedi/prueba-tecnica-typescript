cb075e12949f2e0ee9c16e8aede8484ebdcc8ecd
ChrisCedi <christian.cedillo.palacios@outlook.com>
2024-01-23 19:16:16 -0600

    button to order by country

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,21 @@ import { UserTable } from "./components/UserTable";
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
+  const [sortByCountry, setSortByCountry] = useState(false);
 
   const toggleColors = () => {
     setShowColors(!showColors);
   };
+
+  const handleOrderByCountry = () => {
+    setSortByCountry((prevState) => !prevState);
+  };
+
+  const sortedUsers = sortByCountry
+    ? users.toSorted((a, b) => {
+        return a.location.country.localeCompare(b.location.country);
+      })
+    : users;
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=100")
@@ -24,9 +35,14 @@ function App() {
     <>
       <h1>Lista de usuarios</h1>
       <header>
-        <button onClick={toggleColors}>Mostrar color</button>
+        <button onClick={toggleColors}>
+          {showColors ? `Ocultar colores` : `Mostrar colores`}
+        </button>
+        <button onClick={handleOrderByCountry}>
+          {sortByCountry ? `No ordernar` : `Ordenar por país`}
+        </button>
       </header>
-      <UserTable showColors={showColors} users={users} />
+      <UserTable showColors={showColors} users={sortedUsers} />
     </>
   );
 }
